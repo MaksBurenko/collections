e3483c4104e9e4edddcaf9ae460eb28164c7f1ab
Maks Burenko <Cherniymaks@yandex.ru>
2022-07-19 08:02:59 +0300

    123

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -8,31 +8,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         int number = 0;
-        List <Integer> list = new ArrayList();
+        List <Integer> list1 = new ArrayList();
 
-        InputStream is = System.in;
-        Reader r = new InputStreamReader(is);
-        BufferedReader reader = new BufferedReader(r);
+        Scanner sc = new Scanner(System.in);
 
         OutputStream os = System.out;
         OutputStreamWriter osw = new OutputStreamWriter(os);
         BufferedWriter bw = new BufferedWriter(osw);
 
-        while ((number = reader.read()) != -1) {
-            list.add(number);
+        while (sc.hasNextInt()) {
+            number = sc.nextInt();
+            list1.add(number);
         }
 
-        Set<Integer> set = new HashSet<>();
+        List <Integer> list2 = new ArrayList();
 
-        for (int i = 0 ; i <= list.size(); i++) {
-            if (i % 2 == 0){
-                set.add(list.get(i));
-                set.add(" ");
+        for (int i = 0 ; i < list1.size(); i++) {
+            if (i % 2 != 0){
+                list2.add(list1.get(i));
             }
         }
-        bw.write(String.valueOf(set));
 
-        reader.close();
+        Collections.reverse(list2);
+
+        for (int temp : list2) {
+            bw.write(String.valueOf(temp) + " ");
+        }
+
+        sc.close();
         bw.flush();
         bw.close();
     }
